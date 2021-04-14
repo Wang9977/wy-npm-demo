824784a0ca8649e5095db1cbf07cca1f5ecd56fc
wangyuan198 <wangyuan198@jd.com>
2021-04-14 15:05:48 +0800

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+import moment from './moment'
+export function formatDateTime (value, type) {
+	type = (type === undefined ? 'YYYY-MM-DD' : type)
+	return moment(value).format(type)
+  }
+
+
+function npmDemo(argument) {
+	var name = 'finit';
+	var f1 =function f(arg){console.log(arg)}
+	return {
+		name:name,
+		f1:f1
+	}
+}
+module.exports=npmDemo();
