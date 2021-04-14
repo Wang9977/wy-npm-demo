99ccef91f7f9c354f7650f6408ba6d33d940004c
wangyuan198 <wangyuan198@jd.com>
2021-04-14 17:57:17 +0800

    ok

--- index.mjs
+++ index.mjs
@@ -1,19 +1,17 @@
-// import moment from 'moment'
-// import moment from 'moment'
-const moment = require(moment)
+import moment from 'moment'
 export function formatDateTime (value, type) {
 	type = (type === undefined ? 'YYYY-MM-DD' : type)
 	return moment(value).format(type)
   }
 
 
-// export function npmDemo(argument) {
-// 	var name = 'finit';
-// 	var f1 =function f(arg){console.log(arg)}
-// 	return {
-// 		name:name,
-// 		f1:f1
-// 	}
-// }
+export function npmDemo(argument) {
+	var name = 'finit';
+	var f1 =function f(arg){console.log(arg)}
+	return {
+		name:name,
+		f1:f1
+	}
+}
 // module.exports=npmDemo();
 console.log(formatDateTime(new Date()))
