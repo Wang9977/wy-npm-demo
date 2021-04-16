65c3ae670aec9267cd61324c5bf5d6d62476170e
wangyuan198 <wangyuan198@jd.com>
2021-04-16 16:50:38 +0800

    test: 引用其他函数

--- index.mjs
+++ index.mjs
@@ -15,3 +15,37 @@ export function npmDemo(argument) {
 }
 // module.exports=npmDemo();
 console.log(formatDateTime(new Date()))
+export const unique = (arr) => {
+	// eslint-disable-next-line no-prototype-builtins
+	if (Array.hasOwnProperty('from')) {
+	  return Array.from(new Set(arr))
+	} else {
+	  var n = {}; var r = []
+	  for (var i = 0; i < arr.length; i++) {
+		if (!n[arr[i]]) {
+		  n[arr[i]] = true
+		  r.push(arr[i])
+		}
+	  }
+	  return r
+	}
+  }
+
+
+  export const map = (arr, fn, thisObj) => {
+	var scope = thisObj || window
+	var a = []
+	for (var i = 0, j = arr.length; i < j; ++i) {
+	  var res = fn.call(scope, arr[i], i, this)
+	  if (res != null) a.push(res)
+	}
+	return a
+  }
+
+  export const intersect = (a, b) => {
+	var _this = this
+	a = this.unique(a)
+	return this.map(a, function (o) {
+	  return _this.contains(b, o) ? o : null
+	})
+  }
